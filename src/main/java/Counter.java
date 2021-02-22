d2217969ab4384040f6c70b53f7c5d021b7eeb0e
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-02-23 00:22:29 +0200

    refactored according to comments

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,7 +1,14 @@
+import java.util.Date;
+import org.apache.log4j.Logger;
+
 public class Counter {
+    private static final Logger logger = Logger.getLogger(Counter.class);
     private int num = 0;
 
-    public int increment() {
-        return ++num;
+    public void race() {
+        while (num < Math.sqrt(Math.sqrt(new Date().getTime()))) {
+            ++num;
+            logger.info(Thread.currentThread().getName() + " : " + num);
+        }
     }
 }
--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -1,7 +1,4 @@
-import org.apache.log4j.Logger;
-
 public class MyThread extends Thread {
-    private static final Logger logger = Logger.getLogger(MyThread.class);
     private final Counter counter;
 
     public MyThread(Counter counter) {
@@ -10,8 +7,6 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
-            logger.info(Thread.currentThread().getName() + " : " + counter.increment());
-        }
+        counter.race();
     }
 }
